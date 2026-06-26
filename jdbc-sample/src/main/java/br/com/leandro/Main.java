dbe13422e9167d378454ef6b83f744f94236752f
LeandroMenoci <menoci.leandro@gmail.com>
2026-06-26 20:36:55 -0300

    iniciando com jdbc

---
+++ jdbc-sample/src/main/java/br/com/leandro/Main.java
@@ -0,0 +1,11 @@
+package br.com.leandro;
+
+import br.com.leandro.persistence.EmployeeDAO;
+
+public class Main {
+
+    private final static EmployeeDAO employeeDAO = new EmployeeDAO();
+    public static void main(String[] args) {
+
+    }
+}
---
+++ jdbc-sample/src/main/java/br/com/leandro/persistence/EmployeeDAO.java
@@ -0,0 +1,30 @@
+package br.com.leandro.persistence;
+
+import br.com.leandro.persistence.entity.EmployeeEntity;
+
+import java.util.List;
+
+public class EmployeeDAO {
+
+    public void insert(final EmployeeEntity entity) {
+
+    }
+
+    public void update(final EmployeeEntity entity) {
+
+    }
+
+    public void delete(final long id) {
+
+    }
+
+    public List<EmployeeEntity> findAll() {
+        return null;
+    }
+
+    public EmployeeEntity findById(final long id) {
+        return null;
+    }
+
+
+}
