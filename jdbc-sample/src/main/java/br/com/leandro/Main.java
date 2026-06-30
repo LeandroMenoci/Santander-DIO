5754f9322be2e6829c4e21c8793314a4ed24c2fb
LeandroMenoci <menoci.leandro@gmail.com>
2026-06-30 16:37:19 -0300

    Procedure / EmployeeParamDAO

--- jdbc-sample/src/main/java/br/com/leandro/Main.java
+++ jdbc-sample/src/main/java/br/com/leandro/Main.java
@@ -2,6 +2,7 @@ package br.com.leandro;
 
 import br.com.leandro.persistence.EmployeeAuditDAO;
 import br.com.leandro.persistence.EmployeeDAO;
+import br.com.leandro.persistence.EmployeeParamDAO;
 import br.com.leandro.persistence.entity.EmployeeEntity;
 import org.flywaydb.core.Flyway;
 
@@ -10,7 +11,8 @@ import java.time.OffsetDateTime;
 
 public class Main {
 
-    private final static EmployeeDAO employeeDAO = new EmployeeDAO();
+//    private final static EmployeeDAO employeeDAO = new EmployeeDAO();
+    private final static EmployeeParamDAO employeeParamDAO = new EmployeeParamDAO();
     private final static EmployeeAuditDAO employeeAuditDAO = new EmployeeAuditDAO();
 
 
@@ -22,25 +24,25 @@ public class Main {
 
         flyway.migrate();
 
-//        var employeeInsert = new EmployeeEntity();
-//        employeeInsert.setName("Jose");
-//        employeeInsert.setSalary(new BigDecimal("2600"));
-//        employeeInsert.setBirthday(OffsetDateTime.now().minusYears(56));
-//        System.out.println(employeeInsert);
-//        employeeDAO.insert(employeeInsert);
-//        System.out.println(employeeInsert);
+        var employeeInsert = new EmployeeEntity();
+        employeeInsert.setName("Paloma");
+        employeeInsert.setSalary(new BigDecimal("1100"));
+        employeeInsert.setBirthday(OffsetDateTime.now().minusYears(33));
+        System.out.println(employeeInsert);
+        employeeParamDAO.insertWithProcedure(employeeInsert);
+        System.out.println(employeeInsert);
 //
-        var employeeUpdate = new EmployeeEntity();
-        employeeUpdate.setId(2);
-        employeeUpdate.setName("Leonardo");
-        employeeUpdate.setSalary(new BigDecimal(1200));
-        employeeUpdate.setBirthday(OffsetDateTime.now().minusYears(16));
-        employeeDAO.update(employeeUpdate);
-        System.out.println(employeeUpdate);
-
-        employeeDAO.delete(2);
-
-        employeeAuditDAO.findAll().forEach(System.out::println);
+//        var employeeUpdate = new EmployeeEntity();
+//        employeeUpdate.setId(2);
+//        employeeUpdate.setName("Leonardo");
+//        employeeUpdate.setSalary(new BigDecimal(1200));
+//        employeeUpdate.setBirthday(OffsetDateTime.now().minusYears(16));
+//        employeeParamDAO.update(employeeUpdate);
+//        System.out.println(employeeUpdate);
+//
+//        employeeParamDAO.delete(2);
+//
+//        employeeAuditDAO.findAll().forEach(System.out::println);
 
 
 
